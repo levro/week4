15127c198d6b800978523897de2e70c92f60280c
levro <levro@ukr.net>
2017-03-09 16:53:10 +0200

    next

--- src/com/demo/Demo.java
+++ src/com/demo/Demo.java
@@ -3,6 +3,7 @@ package com.demo;
 public class Demo {
 
 	public static void main(String[] args) {
+
 		
 	}
 
--- src/com/demo/OuterClass.java
+++ src/com/demo/OuterClass.java
@@ -1,6 +1,10 @@
 package com.demo;
 
 public class OuterClass {
+	public static final int pub = 1;
+	public static final int stat = 2;
+	public static final int fin = 3;
+	
 	private class InnerClass{
 		void test(){
 			
